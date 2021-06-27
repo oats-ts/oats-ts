65e780235860fe5cfd618424a267a471357321d4
Balázs Édes <edesbalazs@gmail.com>
2021-06-27 11:15:35 +0200

    fixes example imports

--- projects/example/src/generateAll.ts
+++ projects/example/src/generateAll.ts
@@ -1,16 +1,8 @@
 import { generate } from '@oats-ts/generator'
 import { openAPIReader } from '@oats-ts/openapi-reader'
-import {
-  types,
-  operations,
-  typeGuards,
-  api,
-  validators,
-  parameterTypes,
-  defaultNameProvider,
-} from '@oats-ts/openapi-generator'
+import { types, operations, typeGuards, api, validators, parameterTypes } from '@oats-ts/openapi-generator'
 import { prettierStringify, typeScriptWriter } from '@oats-ts/typescript-writer'
-import { NameProvider, OpenAPIGeneratorTarget } from '@oats-ts/openapi-common'
+import { NameProvider, OpenAPIGeneratorTarget, defaultNameProvider } from '@oats-ts/openapi-common'
 import { join, resolve } from 'path'
 import { readFileSync } from 'fs'
 
