156540544a8946ed5ca4508ea33f3ecf9267735b
Balázs Édes <edesbalazs@gmail.com>
2021-10-23 15:00:55 +0200

    Parameter types moved to separate generators

--- projects/openapi-e2e-test/src/generateOpenAPI.ts
+++ projects/openapi-e2e-test/src/generateOpenAPI.ts
@@ -2,7 +2,12 @@ import { generate, GeneratorConfig } from '@oats-ts/generator'
 import { reader } from '@oats-ts/openapi-reader'
 import { prettierStringify, writer } from '@oats-ts/typescript-writer'
 import { validator } from '@oats-ts/openapi-validator'
-import { parameterTypes } from '@oats-ts/openapi-parameter-types-generator'
+import {
+  queryParameterTypes,
+  pathParameterTypes,
+  requestHeaderParameterTypes,
+  responseHeaderParameterTypes,
+} from '@oats-ts/openapi-parameter-types-generator'
 import { validators } from '@oats-ts/openapi-validators-generator'
 import { types } from '@oats-ts/openapi-types-generator'
 import { apiType } from '@oats-ts/openapi-api-type-generator'
@@ -13,8 +18,16 @@ import { typeGuards } from '@oats-ts/openapi-type-guards-generator'
 import { operations } from '@oats-ts/openapi-operations-generator'
 import { requestTypes } from '@oats-ts/openapi-request-types-generator'
 import { responseTypes } from '@oats-ts/openapi-response-types-generator'
-import { parameterSerializers } from '@oats-ts/openapi-parameter-serializers-generator'
-import { parameterDeserializers } from '@oats-ts/openapi-parameter-deserializers-generator'
+import {
+  pathParameterSerializers,
+  queryParameterSerializers,
+  requestHeaderParameterSerializers,
+} from '@oats-ts/openapi-parameter-serializers-generator'
+import {
+  pathParameterDeserializers,
+  queryParameterDeserializers,
+  requestHeaderParameterDeserializers,
+} from '@oats-ts/openapi-parameter-deserializers-generator'
 import { responseExpectations } from '@oats-ts/openapi-response-expectations-generator'
 import { expressRoute } from '@oats-ts/openapi-express-routes-generator'
 import { nameProviders, pathProviders } from '@oats-ts/openapi'
@@ -52,15 +65,33 @@ export async function generateAll() {
         arrays: true,
         records: true,
       }),
-      parameterTypes({
+      queryParameterTypes({
+        ...common,
+        documentation: true,
+      }),
+      pathParameterTypes({
+        ...common,
+        documentation: true,
+      }),
+      requestHeaderParameterTypes({
+        ...common,
+        documentation: true,
+      }),
+      responseHeaderParameterTypes({
         ...common,
         documentation: true,
       }),
       requestTypes(common),
-      responseTypes(common),
-      parameterSerializers(common),
       responseExpectations(common),
-      parameterDeserializers(common),
+      responseTypes(common),
+      // Serializers
+      pathParameterSerializers(common),
+      queryParameterSerializers(common),
+      requestHeaderParameterSerializers(common),
+      // Deserializers
+      pathParameterDeserializers(common),
+      queryParameterDeserializers(common),
+      requestHeaderParameterDeserializers(common),
       operations({
         ...common,
         validate: true,
