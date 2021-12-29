e8d81d020dbecb069e553d9d5cd735123bca593b
Balázs Édes <edesbalazs@gmail.com>
2021-12-29 13:42:14 +0100

    Label path tests

--- projects/openapi-parameter-deserialization/src/path/path.test.ts
+++ projects/openapi-parameter-deserialization/src/path/path.test.ts
@@ -1,7 +1,10 @@
 import { createPathTestSuite } from './path.testutils'
+import { labelTestData } from './testdata/label.testdata'
+import { labelTestParsers } from './testdata/label.testparsers'
 import { simpleTestData } from './testdata/simple.testdata'
 import { simpleTestParsers } from './testdata/simple.testparsers'
 
 describe('path', () => {
   createPathTestSuite('path.simple', simpleTestParsers, simpleTestData)
+  createPathTestSuite('path.label', labelTestParsers, labelTestData)
 })
---
+++ projects/openapi-parameter-deserialization/src/path/testdata/label.testparsers.ts
@@ -0,0 +1,42 @@
+import { path } from '../index'
+import { PathDeserializers, PathOptions } from '../../types'
+import {
+  enumParser,
+  stringParser,
+  numberParser,
+  booleanParser,
+  literalParser,
+  optionalObjectFieldParsers,
+  objectFieldParsers,
+} from '../../value/testdata/value.testdata'
+import { TestDataObject, TypesObject } from '../../testTypes'
+
+function createTypesParsers(config: PathOptions): TypesObject<PathDeserializers<any>> {
+  return {
+    string: { value: path.label.primitive(stringParser, config) },
+    number: { value: path.label.primitive(numberParser, config) },
+    boolean: { value: path.label.primitive(booleanParser, config) },
+    literal: { value: path.label.primitive(literalParser, config) },
+    enumeration: { value: path.label.primitive(enumParser, config) },
+    array: {
+      string: { value: path.label.array(stringParser, config) },
+      number: { value: path.label.array(numberParser, config) },
+      boolean: { value: path.label.array(booleanParser, config) },
+      literal: { value: path.label.array(literalParser, config) },
+      enumeration: { value: path.label.array(enumParser, config) },
+    },
+    object: {
+      optionalFields: { value: path.label.object(optionalObjectFieldParsers, config) },
+      requiredFields: { value: path.label.object(objectFieldParsers, config) },
+    },
+  }
+}
+
+export const labelTestParsers: TestDataObject<PathDeserializers<any>> = {
+  explode: {
+    required: createTypesParsers({ explode: true }),
+  },
+  noExplode: {
+    required: createTypesParsers({ explode: false }),
+  },
+}
