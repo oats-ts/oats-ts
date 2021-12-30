a8fc4c55de8c1e3a8b6f24b0a278257287725273
Balázs Édes <edesbalazs@gmail.com>
2021-12-30 20:32:26 +0100

    Better message for getData

--- projects/try/src/getData.ts
+++ projects/try/src/getData.ts
@@ -1,9 +1,10 @@
+import { stringify } from '@oats-ts/validators'
 import { isFailure } from './isFailure'
 import { Try } from './types'
 
 export function getData<T>(input: Try<T>): T {
   if (isFailure(input)) {
-    throw new TypeError(`Can't get value of Failure (${input})`)
+    throw new TypeError(input.issues.map(stringify).join('\n'))
   }
   return input.data
 }
