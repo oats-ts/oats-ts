13c1f91b65fc5fa6f881288093f3bc0c39f1dcfd
Balázs Édes <edesbalazs@gmail.com>
2022-03-14 22:56:34 +0100

    typings improvements

--- projects/openapi-request-types-generator/src/common/getCommonImports.ts
+++ projects/openapi-request-types-generator/src/common/getCommonImports.ts
@@ -1,8 +1,7 @@
 import { flatMap, isNil, negate, values } from 'lodash'
-import { getRequestBodyContent, RuntimePackages } from '@oats-ts/openapi-common'
+import { getRequestBodyContent } from '@oats-ts/openapi-common'
 import { OpenAPIGeneratorContext } from '@oats-ts/openapi-common'
 import { EnhancedOperation } from '@oats-ts/openapi-common'
-import { getNamedImports } from '@oats-ts/typescript-common'
 
 export function getCommonImports(path: string, data: EnhancedOperation, context: OpenAPIGeneratorContext) {
   const bodies = values(getRequestBodyContent(data, context))
@@ -13,12 +12,6 @@ export function getCommonImports(path: string, data: EnhancedOperation, context:
   const { operation } = data
 
   return [
-    getNamedImports(RuntimePackages.Http.name, [
-      ...(bodies.length > 0 ? [RuntimePackages.Http.HasRequestBody] : []),
-      ...(data.path.length > 0 ? [RuntimePackages.Http.HasPathParameters] : []),
-      ...(data.query.length > 0 ? [RuntimePackages.Http.HasQueryParameters] : []),
-      ...(data.header.length > 0 ? [RuntimePackages.Http.HasHeaders] : []),
-    ]),
     ...flatMap(bodies, (schema) => dependenciesOf(path, schema, 'json-schema/type')),
     ...dependenciesOf(path, operation, 'openapi/path-type'),
     ...dependenciesOf(path, operation, 'openapi/query-type'),
