fcbd27ddb6007803d463a44b75dbc956e13a20f7
unknown <edesbalazs@gmail.com>
2022-07-20 21:12:22 +0200

    #79 Validator now emits non-critical issues

--- projects/openapi-validator/src/validator.ts
+++ projects/openapi-validator/src/validator.ts
@@ -3,10 +3,13 @@ import { OpenAPIReadOutput } from '@oats-ts/openapi-reader'
 import { OpenAPIValidatorConfig } from './typings'
 import { createOpenAPIValidatorConfig } from './createOpenAPIValidatorConfig'
 import { OpenAPIObject } from '@oats-ts/openapi-model'
-import { fluent, fromArray, fromPromiseSettledResult, isSuccess, Try } from '@oats-ts/try'
+import { failure, fluent, fromArray, fromPromiseSettledResult, isSuccess, success, Try } from '@oats-ts/try'
 import { tick } from './utils/tick'
 import { validateDocument } from './validateDocument'
 import { OpenAPIValidatorContextImpl } from './OpenApiValidatorContextImpl'
+import { flatMap } from 'lodash'
+import { severityComparator } from './severityComparator'
+import { isOk } from '@oats-ts/validators'
 
 const name = '@oats-ts/openapi-validator'
 
@@ -29,16 +32,17 @@ export function validator(
     const validationResult = await Promise.allSettled(
       context.documents.map((document) => validateDocument(document, context, config, emitter)),
     )
-    const results = fluent(
-      fromArray(validationResult.map(fromPromiseSettledResult).map((wrapped) => fluent(wrapped).flatMap((t) => t))),
-    ).map(() => data)
-
+    const results = fluent(fromArray(validationResult.map(fromPromiseSettledResult))).map((data) =>
+      flatMap(data).sort(severityComparator),
+    )
+    const allIssues = Array.from(isSuccess(results) ? results.data : results.issues).sort(severityComparator)
+    const hasNoCriticalIssues = isOk(allIssues, ['error'])
     emitter.emit('validator-step-completed', {
       type: 'validate-step-completed',
       name,
-      issues: isSuccess(results) ? [] : results.issues,
+      issues: allIssues,
     })
 
-    return results
+    return hasNoCriticalIssues ? success(data) : failure(allIssues)
   }
 }
