afc7c1671b11e79de5190e6df4f2ba91e0f34369
unknown <edesbalazs@gmail.com>
2022-10-09 15:41:55 +0200

    Refactored API and context handler generator

--- projects/openapi-generators/src/api-type/ApiTypeGenerator.ts
+++ projects/openapi-generators/src/api-type/ApiTypeGenerator.ts
@@ -1,13 +1,21 @@
-import { EnhancedOperation, OpenAPIGeneratorTarget } from '@oats-ts/openapi-common'
+import { EnhancedOperation, hasInput, OpenAPIGeneratorTarget } from '@oats-ts/openapi-common'
 import { OpenAPIObject } from '@oats-ts/openapi-model'
-import { factory, ImportDeclaration, SourceFile, TypeReferenceNode } from 'typescript'
+import {
+  factory,
+  ImportDeclaration,
+  MethodSignature,
+  ParameterDeclaration,
+  SourceFile,
+  Statement,
+  SyntaxKind,
+  TypeReferenceNode,
+} from 'typescript'
 import { success, Try } from '@oats-ts/try'
-import { createSourceFile, getModelImports } from '@oats-ts/typescript-common'
+import { createSourceFile, documentNode, getModelImports } from '@oats-ts/typescript-common'
 import { ApiTypeGeneratorConfig } from './typings'
-import { getApiTypeImports } from './getApiTypeImports'
-import { getApiTypeAst } from './getApiTypeAst'
 import { DocumentBasedCodeGenerator } from '../utils/DocumentBasedCodeGenerator'
 import { RuntimeDependency } from '@oats-ts/oats-ts'
+import { flatMap } from 'lodash'
 
 export class ApiTypeGenerator extends DocumentBasedCodeGenerator<ApiTypeGeneratorConfig> {
   public name(): OpenAPIGeneratorTarget {
@@ -22,16 +30,6 @@ export class ApiTypeGenerator extends DocumentBasedCodeGenerator<ApiTypeGenerato
     return []
   }
 
-  public async generateItem(operations: EnhancedOperation[]): Promise<Try<SourceFile>> {
-    return success(
-      createSourceFile(
-        this.context.pathOf(this.input.document, this.name()),
-        getApiTypeImports(this.input.document, operations, this.context, true),
-        [getApiTypeAst(this.input.document, operations, this.context, this.configuration())],
-      ),
-    )
-  }
-
   public referenceOf(input: OpenAPIObject): TypeReferenceNode | undefined {
     const [operations] = this.items
     return operations.length > 0 ? factory.createTypeReferenceNode(this.context.nameOf(input, this.name())) : undefined
@@ -40,5 +38,63 @@ export class ApiTypeGenerator extends DocumentBasedCodeGenerator<ApiTypeGenerato
   public dependenciesOf(fromPath: string, input: OpenAPIObject): ImportDeclaration[] {
     const [operations] = this.items
     return operations.length > 0 ? getModelImports(fromPath, this.name(), [input], this.context) : []
+  }
+
+  public async generateItem(operations: EnhancedOperation[]): Promise<Try<SourceFile>> {
+    return success(
+      createSourceFile(
+        this.context.pathOf(this.context.document, this.name()),
+        this.getImportDeclarations(operations),
+        [this.getApiTypeStatement(operations)],
+      ),
+    )
+  }
+
+  protected getImportDeclarations(operations: EnhancedOperation[]): ImportDeclaration[] {
+    const apiPath = this.context.pathOf(this.context.document, this.name())
+    return flatMap(operations, (data) => [
+      ...this.context.dependenciesOf(apiPath, data.operation, 'oats/request-server-type'),
+      ...this.context.dependenciesOf(apiPath, data.operation, 'oats/response-server-type'),
+    ])
+  }
+
+  protected getApiTypeStatement(operations: EnhancedOperation[]): Statement {
+    return factory.createTypeAliasDeclaration(
+      [],
+      [factory.createModifier(SyntaxKind.ExportKeyword)],
+      this.context.nameOf(this.context.document, this.name()),
+      [],
+      factory.createTypeLiteralNode(operations.map((operation) => this.getApiTypeMethodSignatureAst(operation))),
+    )
+  }
+
+  protected getApiTypeMethodSignatureAst(data: EnhancedOperation): MethodSignature {
+    const parameters: ParameterDeclaration[] = hasInput(data, this.context, true)
+      ? [
+          factory.createParameterDeclaration(
+            [],
+            [],
+            undefined,
+            'request',
+            undefined,
+            this.context.referenceOf(data.operation, 'oats/request-server-type'),
+          ),
+        ]
+      : []
+
+    const returnType = factory.createTypeReferenceNode('Promise', [
+      factory.createTypeReferenceNode(this.context.nameOf(data.operation, 'oats/response-server-type')),
+    ])
+
+    const node = factory.createMethodSignature(
+      [],
+      this.context.nameOf(data.operation, 'oats/operation'),
+      undefined,
+      [],
+      parameters,
+      returnType,
+    )
+
+    return this.configuration().documentation ? documentNode(node, data.operation) : node
   }
 }
--- projects/openapi-generators/src/express-context-handler-factory/ExpressContextHandlerFactoryGenerator.ts
+++ projects/openapi-generators/src/express-context-handler-factory/ExpressContextHandlerFactoryGenerator.ts
@@ -1,11 +1,20 @@
-import { EnhancedOperation, OpenAPIGeneratorTarget, RuntimePackages } from '@oats-ts/openapi-common'
+import { OpenAPIGeneratorTarget, RuntimePackages } from '@oats-ts/openapi-common'
 import { OpenAPIObject } from '@oats-ts/openapi-model'
-import { TypeNode, Expression, factory, ImportDeclaration, SourceFile } from 'typescript'
+import {
+  TypeNode,
+  Expression,
+  factory,
+  ImportDeclaration,
+  SourceFile,
+  ParameterDeclaration,
+  SyntaxKind,
+  Statement,
+} from 'typescript'
 import { createSourceFile, getModelImports, getNamedImports } from '@oats-ts/typescript-common'
 import { success, Try } from '@oats-ts/try'
-import { getRouterFactoryAst } from './getRouterFactoryAst'
 import { DocumentBasedCodeGenerator } from '../utils/DocumentBasedCodeGenerator'
 import { RuntimeDependency, version } from '@oats-ts/oats-ts'
+import { RouterNames } from '../utils/express/RouterNames'
 
 export class ExpressContextHandlerFactoryGenerator extends DocumentBasedCodeGenerator<{}> {
   public name(): OpenAPIGeneratorTarget {
@@ -22,27 +31,6 @@ export class ExpressContextHandlerFactoryGenerator extends DocumentBasedCodeGene
     ]
   }
 
-  protected async generateItem(operations: EnhancedOperation[]): Promise<Try<SourceFile>> {
-    const path = this.context.pathOf(this.input.document, this.name())
-    return success(
-      createSourceFile(
-        path,
-        [
-          getNamedImports(RuntimePackages.Express.name, [
-            RuntimePackages.Express.Handler,
-            RuntimePackages.Express.NextFunction,
-            RuntimePackages.Express.Request,
-            RuntimePackages.Express.Response,
-          ]),
-          getNamedImports(RuntimePackages.Http.name, [RuntimePackages.Http.ServerAdapter]),
-          getNamedImports(RuntimePackages.HttpServerExpress.name, [RuntimePackages.HttpServerExpress.ExpressToolkit]),
-          ...getModelImports<OpenAPIGeneratorTarget>(path, 'oats/api-type', [this.input.document], this.context),
-        ],
-        [getRouterFactoryAst(operations, this.context)],
-      ),
-    )
-  }
-
   public referenceOf(input: OpenAPIObject): TypeNode | Expression | undefined {
     const [operations] = this.items
     return operations?.length > 0 ? factory.createTypeReferenceNode(this.context.nameOf(input, this.name())) : undefined
@@ -51,5 +39,144 @@ export class ExpressContextHandlerFactoryGenerator extends DocumentBasedCodeGene
   public dependenciesOf(fromPath: string, input: OpenAPIObject): ImportDeclaration[] {
     const [operations] = this.items
     return operations?.length > 0 ? getModelImports(fromPath, this.name(), [input], this.context) : []
+  }
+
+  protected async generateItem(): Promise<Try<SourceFile>> {
+    const path = this.context.pathOf(this.input.document, this.name())
+    return success(createSourceFile(path, this.getImportDeclarations(path), [this.getRouterFactoryAst()]))
+  }
+
+  protected getImportDeclarations(path: string): ImportDeclaration[] {
+    return [
+      getNamedImports(RuntimePackages.Express.name, [
+        RuntimePackages.Express.Handler,
+        RuntimePackages.Express.NextFunction,
+        RuntimePackages.Express.Request,
+        RuntimePackages.Express.Response,
+      ]),
+      getNamedImports(RuntimePackages.Http.name, [RuntimePackages.Http.ServerAdapter]),
+      getNamedImports(RuntimePackages.HttpServerExpress.name, [RuntimePackages.HttpServerExpress.ExpressToolkit]),
+      ...getModelImports<OpenAPIGeneratorTarget>(path, 'oats/api-type', [this.input.document], this.context),
+    ]
+  }
+
+  protected getHandlerParamteters(): ParameterDeclaration[] {
+    return [
+      factory.createParameterDeclaration(
+        undefined,
+        undefined,
+        undefined,
+        factory.createIdentifier('_'),
+        undefined,
+        factory.createTypeReferenceNode(factory.createIdentifier(RuntimePackages.Express.Request), undefined),
+        undefined,
+      ),
+      factory.createParameterDeclaration(
+        undefined,
+        undefined,
+        undefined,
+        factory.createIdentifier(RouterNames.response),
+        undefined,
+        factory.createTypeReferenceNode(factory.createIdentifier(RuntimePackages.Express.Response), undefined),
+        undefined,
+      ),
+      factory.createParameterDeclaration(
+        undefined,
+        undefined,
+        undefined,
+        factory.createIdentifier(RouterNames.next),
+        undefined,
+        factory.createTypeReferenceNode(factory.createIdentifier(RuntimePackages.Express.NextFunction), undefined),
+        undefined,
+      ),
+    ]
+  }
+
+  protected getFactoryParameters(): ParameterDeclaration[] {
+    return [
+      factory.createParameterDeclaration(
+        undefined,
+        undefined,
+        undefined,
+        factory.createIdentifier(RouterNames.api),
+        undefined,
+        factory.createTypeReferenceNode(this.context.referenceOf(this.context.document, 'oats/api-type')),
+        undefined,
+      ),
+      factory.createParameterDeclaration(
+        undefined,
+        undefined,
+        undefined,
+        factory.createIdentifier(RouterNames.adapter),
+        undefined,
+        factory.createTypeReferenceNode(factory.createIdentifier(RuntimePackages.Http.ServerAdapter), [
+          factory.createTypeReferenceNode(
+            factory.createIdentifier(RuntimePackages.HttpServerExpress.ExpressToolkit),
+            undefined,
+          ),
+        ]),
+        undefined,
+      ),
+    ]
+  }
+
+  protected getRouterFactoryAst(): Statement {
+    return factory.createFunctionDeclaration(
+      undefined,
+      [factory.createModifier(SyntaxKind.ExportKeyword)],
+      undefined,
+      factory.createIdentifier(this.context.nameOf(this.context.document, this.name())),
+      undefined,
+      this.getFactoryParameters(),
+      factory.createTypeReferenceNode(factory.createIdentifier(RuntimePackages.Express.Handler), undefined),
+      factory.createBlock(
+        [
+          factory.createReturnStatement(
+            factory.createArrowFunction(
+              undefined,
+              undefined,
+              this.getHandlerParamteters(),
+              undefined,
+              factory.createToken(SyntaxKind.EqualsGreaterThanToken),
+              factory.createBlock(
+                [
+                  factory.createExpressionStatement(
+                    factory.createBinaryExpression(
+                      factory.createElementAccessExpression(
+                        factory.createPropertyAccessExpression(
+                          factory.createIdentifier(RouterNames.response),
+                          factory.createIdentifier(RouterNames.locals),
+                        ),
+                        factory.createStringLiteral(RouterNames.apiKey(this.context.hashOf(this.context.document))),
+                      ),
+                      factory.createToken(SyntaxKind.EqualsToken),
+                      factory.createIdentifier(RouterNames.api),
+                    ),
+                  ),
+                  factory.createExpressionStatement(
+                    factory.createBinaryExpression(
+                      factory.createElementAccessExpression(
+                        factory.createPropertyAccessExpression(
+                          factory.createIdentifier(RouterNames.response),
+                          factory.createIdentifier(RouterNames.locals),
+                        ),
+                        factory.createStringLiteral(RouterNames.adapterKey(this.context.hashOf(this.context.document))),
+                      ),
+                      factory.createToken(SyntaxKind.EqualsToken),
+                      factory.createIdentifier(RouterNames.adapter),
+                    ),
+                  ),
+                  factory.createExpressionStatement(
+                    factory.createCallExpression(factory.createIdentifier(RouterNames.next), undefined, []),
+                  ),
+                ],
+                true,
+              ),
+            ),
+          ),
+        ],
+        true,
+      ),
+    )
   }
 }
